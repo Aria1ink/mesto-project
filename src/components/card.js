7a550228d9e9cd678c4b495e81570b08b8211aa1
Aria1ink <ann.arialink@yandex.ru>
2022-11-04 21:41:23 +0300

    лайки ворк

--- src/components/card.js
+++ src/components/card.js
@@ -4,34 +4,35 @@ import { connectionData } from './data.js';
 //cards
 const cardContainer = document.querySelector('.element');
 function countLikes (card) {
-  const likes = {
-    likeSum: 0,
-    iLikeIt: false
-  };
-  if (card.likes.lenght != 0) {
-    likes.iLikeIt = card.likes.some(like => like._id == '43a35a073393c920572f7de1')
-    likes.likeSum = card.likes.length;
-  };
-  return likes;
+  return card.likes.length;
 };
+function isILikeIT (card) {
+  return card.likes.some(like => like._id == '43a35a073393c920572f7de1');
+}
+function writeLikeCount (cardLikesCounter, likeSum) {
+  cardLikesCounter.textContent = likeSum;
+}
 // формирование шаблона карточки
 function getCard (item) {
   const cardTemplate = document.querySelector('#cardTemplate').content;
   const card = cardTemplate.querySelector('.element__card').cloneNode(true);
   const cardImage = card.querySelector('.element__image');
-  const cardLikesCount = card.querySelector('.element__like-counter');
-  const cardLikes = countLikes(item);
+  const cardLikesCounter = card.querySelector('.element__like-counter');
+  const cardLike = card.querySelector('.element__like');
   card.querySelector('.element__title').textContent = item.name;
   cardImage.src = item.link;
   cardImage.alt = item.name;
-  card.querySelector('.like').addEventListener('click', evt => {addLike(evt, item._id);});
+  card.querySelector('.like').addEventListener('click', evt => {addLike(evt, item._id, cardLike, cardLikesCounter);});
   if (item.owner._id == '43a35a073393c920572f7de1') {
     card.querySelector('.element__remove').addEventListener('click', removeCard);
   } else {
     card.removeChild(card.querySelector('.element__remove'));
   };
   cardImage.addEventListener('click', (evt) => {openImage(cardImage.src, cardImage.alt);});
-  cardLikesCount.textContent = cardLikes.likeSum;
+  if (isILikeIT(item)) {
+    toggleLike(cardLike);
+  }
+  writeLikeCount(cardLikesCounter, countLikes(item));
   return card;
 };
 // вставка на страницу
@@ -39,7 +40,6 @@ export function createCard (item) {
   if (item) {
     const card = getCard(item);
     cardContainer.prepend(card);
-    console.log(item);
   };
 };
 // удаление карточки
@@ -47,14 +47,12 @@ function removeCard (evt) {
   evt.target.closest('.element__card').remove();
 };
 // лайки
-function addLike (evt, cardID) {
-  evt.preventDefault();
-  console.log(cardID);
+function addLike ( evt, cardID, cardLike, cardLikesCounter ) {
   if (evt.target.classList.contains('like_status_active')) {
     setCardLike(connectionData, cardID, 'DELETE')
       .then(checkPromiseResult)
       .then(card => {
-        console.log(card);
+        writeLikeCount(cardLikesCounter, countLikes(card));
       })
       .catch(err => {
         console.log(err);
@@ -63,12 +61,14 @@ function addLike (evt, cardID) {
     setCardLike(connectionData, cardID, 'PUT')
       .then(checkPromiseResult)
       .then(card => {
-        countLikes(card);
-
+        writeLikeCount(cardLikesCounter, countLikes(card));
       })
       .catch(err => {
         console.log(err);
       })
   };
-  evt.target.classList.toggle('like_status_active');
+  toggleLike (cardLike);
 };
+function toggleLike ( cardLike) {
+  cardLike.classList.toggle('like_status_active');
+}
